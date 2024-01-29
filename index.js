04a75a0a253b0ce850439eb218b009b538a70a58
soufiane-svr <nwigashop@gmail.com>
2024-01-29 21:56:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const UserModel = require('./models/Users')
 
 
 app.get('/users',async (req,res)=>{
-    const user =  await UserModel.find().maxTimeMS(30000);
+    const user =  await UserModel.find().maxTimeMS(100000);
     res.json(user)
 })
 
